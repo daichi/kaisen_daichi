12658c37cbb53b5b3dba3fc07cd4c87a35ed87a5
daichi <badominton10@hotmail.co.jp>
2010-10-05 11:08:43 +0900

    rails new kaisen_daichi

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KaisenDaichi::Application.config.secret_token = '6eeb9d4d1315c09854de6bdca72a6ae656a6042690c5e2b6c4d1732374a664ca9da3665b725ff1b6d44b12fdc685b1bc25402f5cb966a45e4e786b011c15b54e'
