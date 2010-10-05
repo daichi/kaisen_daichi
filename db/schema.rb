c78d3ecc23e9a447736d11201d2439d676381e6c
daichi <badominton10@hotmail.co.jp>
2010-10-05 13:53:59 +0900

    add price to product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101005024712) do
+ActiveRecord::Schema.define(:version => 20101005042308) do
 
   create_table "products", :force => true do |t|
     t.string   "neme"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(:version => 20101005024712) do
     t.string   "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "price",       :default => 0
   end
 
 end
