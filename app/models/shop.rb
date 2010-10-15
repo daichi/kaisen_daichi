c5763c2e1f48921b2198442f4c61564aeadfa83c
daichi <badominton10@hotmail.co.jp>
2010-10-15 15:50:15 +0900

    add validation to product

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,5 +2,5 @@ class Shop < ActiveRecord::Base
   validates_presence_of :name, :description, :lines_summary
   validates_format_of :image_url, :with => %r{\.(gif|jpg|png)$}i,
  		      :message => "はGIF,JPG,PNG画像でなければなりません"
-  validates_uniqeness_of :name
+  validates_uniqueness_of :name
 end
