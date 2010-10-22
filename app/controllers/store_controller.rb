a9451947db1f6718fcc683f7607e1e6e44a1f16b
daichi <badominton10@hotmail.co.jp>
2010-10-22 17:04:54 +0900

    add StoreController#empty_cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,17 +1,23 @@
 class StoreController < ApplicationController
   def index
     #@cart = current_cart
-    @products = Product.for_sale.paginate :page => params[:page], :per_page => 2   
+    @products = Product.for_sale.paginate :page => params[:page], :per_page => 5
   end
 
   def add_to_cart
     @product = Product.find(params[:id])
     @cart = current_cart
     @cart.add_product(@product)
+    flash[:notice] = "#{@product.name}が買い物カゴに追加されました"
   end
 
+  def empty_cart
+    session[:cart] = nil
+    redirect_to store_path, :notice => "カートは現在空です"
+  end 
+
   private
-    def current_cart
-      session[:cart] ||= Cart.new
-    end
+  def current_cart
+    session[:cart] ||= Cart.new
+  end
 end
---
+++ app/models/cart.rb
@@ -0,0 +1,15 @@
+class Cart
+  def add_product(product)
+    current_item = items.find{|item| item.product_id == product.id}
+    if current_item
+      current_item.increment_quantity
+    else
+      items << CartItem.new(product)
+    end
+  end
+  
+  def items
+    @items ||= []
+  end
+
+end
